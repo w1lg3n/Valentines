df6b9160dc06a860e75bf5b00e477fe641f4fac9
w1lg3n <melendewilgen@gmail.com>
2024-02-16 20:07:13 +0800

    version 1

---
+++ Khiana.js
@@ -0,0 +1,23 @@
+document.getElementById('myImage1').onclick = function() {
+    document.getElementById('popup1').style.display = "block";
+  }
+  
+  document.getElementById('close1').onclick = function() {
+    document.getElementById('popup1').style.display = "none";
+  }
+
+  document.getElementById('myImage2').onclick = function() {
+    document.getElementById('popup2').style.display = "block";
+  }
+  
+  document.getElementById('close2').onclick = function() {
+    document.getElementById('popup2').style.display = "none";
+  }
+
+  document.getElementById('myImage3').onclick = function() {
+    document.getElementById('popup3').style.display = "block";
+  }
+  
+  document.getElementById('close3').onclick = function() {
+    document.getElementById('popup3').style.display = "none";
+  }
